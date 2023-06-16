b01e668954b22a29ae9a276c0461318ad6e7ccf0
Ali-Azimi00 <alidotazimi@gmail.com>
2023-06-15 18:07:59 -0700

    first commit

---
+++ src/main/java/com/revature/AJReimbursements/AjReimbursementsApplication.java
@@ -0,0 +1,13 @@
+package com.revature.AJReimbursements;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjReimbursementsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjReimbursementsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revature/AJReimbursements/AjReimbursementsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.AJReimbursements;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjReimbursementsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
